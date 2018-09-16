77a94ed498ee850bf8993352bb397406bd437d6a
harshit0810 <harshitaf0810@gmail.com>
2018-09-16 21:04:54 +0530

    this is my second assignment.

---
+++ Assignment-2/solution.py
@@ -0,0 +1,35 @@
+#QUESTION-1
+print("anything")
+
+
+#QUESTION-2
+str1='ONE '
+str2='PIECE'
+print(str1+str2)
+
+
+#QUESTION-3
+x=input("enter a value:- ")
+y=input("enter a value:- ")
+z=input("enter a value:- ")
+print(x)
+print(y)
+print(z)
+
+
+#QUESTION-4
+print("Let's get started")
+
+
+#QUESTION-5
+s="Acadview"
+course="Python"
+fees=5000
+print("%s %s %d" %(s,course,fees))
+
+
+#QUESTION-6
+pi=3.14
+radius=int(input("enter the radius of the circle:- "))
+area=pi*radius*radius
+print(area)
