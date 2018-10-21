a85ca00dce478b54d157e6d716c360b84b7be9ce
harshit0810 <harshitaf0810@gmail.com>
2018-10-21 22:07:56 +0530

    Updated

--- Assignment-10/solutions.py
+++
@@ -1,4 +0,0 @@
-#QUESTION-1
-file=open("file.txt","r")
-n=int(input("enter no. of lines:- "))
-print(file.readlines()[0:n])
--- Assignment-12/solutions.py
+++
@@ -1,9 +0,0 @@
-#QUESTION-1
-import datetime
-print(datetime.datetime.now().strftime("%aday"))
-
-#QUESTION-2
-import webbrowser
-webbrowser.open("https://www.youtube.com/watch?v=cwQgjq0mCdE", new=2)
-
-#QUESTION-3
